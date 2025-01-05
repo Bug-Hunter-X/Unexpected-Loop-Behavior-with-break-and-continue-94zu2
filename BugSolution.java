856849bed93dcab1f3ddaa623777c437da7a56d0
BugHunter-X <askzenai@gmail.com>
2025-01-05 21:58:39 +0100

    Add BugSolution.java file

---
+++ BugSolution.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            if (i == 5) {
+                break; // Exit the loop when i is 5
+            }
+            if (i == 7) {
+                continue; // Skip to the next iteration when i is 7
+            }
+            i++;
+        }
+    }
+}
